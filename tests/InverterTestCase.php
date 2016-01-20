980cab2912bb7cdd786976f480c6cbcd79eadce2
Dave Smith-Hayes <dsmithhayes@gmail.com>
2016-01-19 21:20:36 -0500

    I think this is good enough

--- tests/InverterTestCase.php
+++ tests/InverterTestCase.php
@@ -4,36 +4,52 @@ use Qool\Inverter;
 
 class InverterTestCase extends PHPUnit_Framework_TestCase
 {
-    public function testTrueInverter()
+    public function testTrue()
     {
         $this->assertTrue(Inverter::invert(false));
     }
 
-    public function testFalseInverter()
+    public function testFalse()
     {
         $this->assertFalse(Inverter::invert(true));
     }
 
-    public function testNullInverter()
+    public function testNull()
     {
         $this->assertTrue(Inverter::invert(null));
     }
 
-    public function testOneIntInverter()
+    public function testOneInt()
     {
         $this->assertFalse(Inverter::invert(1));
     }
 
+    public function testZeroInt()
+    {
+        $this->assertTrue(Inverter::invert(0));
+    }
+
     /**
-     * Note that -1 negated is still true.
+     * Note that -1 negated is still false.
      */
     public function testNegativeInt()
     {
         $this->assertFalse(Inverter::invert(-1));
     }
 
+    /**
+     * Negated floats are false, also!
+     */
     public function testFloatInverter()
     {
         $this->assertFalse(Inverter::invert(0.1));
+    }
+
+    /**
+     * Negated objects are always false!
+     */
+    public function testObjectInverter()
+    {
+        $this->assertFalse(Inverter::invert(new stdClass()));
     }
 }
