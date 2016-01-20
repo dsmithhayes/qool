d52a5ac940a7f79be6adf7df874eab454eced6e3
Dave Smith-Hayes <dsmithhayes@gmail.com>
2016-01-19 22:35:36 -0500

    One of the reasons I'm even building this library, the twelve laws.

---
+++ tests/TwelveLawsTestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+use Qool\Inverter;
+use Qool\Gate\Qand;
+use Qool\Gate\Qnand;
+use Qool\Gate\Qnor;
+use Qool\Gate\Qor;
+use Qool\Gate\Qxor;
+
+/**
+ * The reason I built this library: the twelve rules of Boolean.
+ */
+class TwelveLawsTestCase extends PHPUnit_Framework_TestCase
+{
+
+}
